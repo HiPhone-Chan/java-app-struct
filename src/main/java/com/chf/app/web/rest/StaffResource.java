0b12dd026085ca0b1bd5b51aa7982aee79117869
chf <hi.phone.chan@gmail.com>
2021-10-01 15:40:45 +0800

    merge master修改

--- src/main/java/com/chf/app/web/rest/StaffResource.java
+++ src/main/java/com/chf/app/web/rest/StaffResource.java
@@ -95,7 +95,7 @@ public class StaffResource {
         User user = Optional.of(login).flatMap(userRepository::findOneByLogin).orElseThrow();
         if (user.getAuthorities().stream().map(Authority::getName).collect(Collectors.toSet())
                 .contains(AuthoritiesConstants.STAFF)) {
-            userService.changePasswordBySuperior(login, passwordChangeDTO.getCurrentPassword(),
+            userService.changePasswordBySuperior(user, passwordChangeDTO.getCurrentPassword(),
                     passwordChangeDTO.getNewPassword());
         }
     }
